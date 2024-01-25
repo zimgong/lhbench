2cb18ee48ccbbd8bbfa150e69e93b0ad4c909085
Zim Gong <zimgong@umich.edu>
2024-01-24 19:54:16 -0500

    add todos

--- data_collector.sh
+++ data_collector.sh
@@ -7,6 +7,8 @@ ssh_user=$(whoami)
 
 echo "Welcome $ssh_user"
 
+# TODO: Check the consecutive IPs to determine which nodes are available
+
 # Get the user's IP address
 cluster_hostname=$(ifconfig -a | grep -oP 'inet \K10\.164\.\d{1,3}\.\d{1,3}' | head -n1)
 pem_file="/home/$ssh_user/.ssh/id_rsa"
@@ -31,7 +33,12 @@ else
     pkill -f metastore
     cd /home/$ssh_user/mysql
 
+    # TODO: Fix MySQL shutdown and restart issue. Currently cannot login again with the old password
+
     if [ -S "/home/$ssh_user/mysql/mysql.sock" ]; then
+        # rm ./mysql.sock.lock
+        # rm ./mysql.sock
+        # rm ./mysql.pid
         bin/mysqladmin -uroot -p shutdown -S mysql.sock --password=111
         rm -rf ~/data
     fi
@@ -90,7 +97,7 @@ else
     bin/mysqld --defaults-file=my.cnf --initialize
     password=$(grep -oP 'root@localhost:\K.*' log/*.log | tail -n 1)
     bin/mysqld_safe --defaults-file=my.cnf &
-    mysql --defaults-file=my.cnf -u root -p"${password}" < set_mysql.txt
+    mysql --defaults-file=my.cnf -u root -p"${password}" < /home/${ssh_user}/lhbench/set_mysql.txt
 
     schematool -initSchema -dbType mysql -vebin/mysqld_safe --defaults-file=my.cnf
     hive --service metastore &
@@ -100,68 +107,69 @@ fi
 
 cd /home/$ssh_user/lhbench
 
-# # Define the user-defined jobs
-# job_counter=1
-# STOP_AFTER=1
-# while IFS=' ' read -r engine test rest_of_line; do
-#     # Check if the engine type is valid
-#     if [[ "$engine" == "delta" || "$engine" == "iceberg" || "$engine" == "hudi" ]]; then
-#         # Construct the job command based on the engine type and the rest of the line
-#         if [[ "$test == tpcds" ]]; then
-#             load_command="./run-benchmark.py \
-#                 --cluster-hostname ${cluster_hostname} \
-#                 -i ${pem_file} \
-#                 --ssh-user ${ssh_user} \
-#                 --source-path ${source_path} \
-#                 --benchmark-path /output/load-1gb \
-#                 --benchmark tpcds-${rest_of_line}-${engine}-load"
-#             query_command="./run-benchmark.py \
-#                 --cluster-hostname ${cluster_hostname} \
-#                 -i ${pem_file} \
-#                 --ssh-user ${ssh_user} \
-#                 --benchmark-path /output/load-1gb \
-#                 --benchmark tpcds-${rest_of_line}-${engine}"
-#             echo "Job $job_counter: $engine $test $rest_of_line"
-#             $load_command
-#             $query_command
-#         elif [[ "$test == refresh" ]]; then
-#             strategy=$rest_of_line[0]
-#             run_command="./run-benchmark.py \
-#                 --cluster-hostname ${cluster_hostname} \
-#                 -i ${pem_file} \
-#                 --ssh-user ${ssh_user}"
-#             echo "Job $job_counter: $engine $test $rest_of_line"
-#             $run_command
-#         elif [[ "$test == merge" ]]; then
-#             echo "Job $job_counter: $engine $test $rest_of_line"
-#         elif [[ "$test == count" ]]; then
-#             load_command="./run-benchmark.py \
-#                 --cluster-hostname ${cluster_hostname} \
-#                 -i ${pem_file} \
-#                 --ssh-user ${ssh_user} \
-#                 --benchmark-path /output/sorted-${rest_of_line}-${engine} \
-#                 --benchmark ${rest_of_line}-files-sorted-${engine}-load"
-#             query_command="./run-benchmark.py \
-#                 --cluster-hostname ${cluster_hostname} \
-#                 -i ${pem_file} \
-#                 --ssh-user ${ssh_user} \
-#                 --benchmark-path /output/sorted-${rest_of_line}-${engine} \
-#                 --cloud-provider hdfs \
-#                 --benchmark ${rest_of_line}-files-sorted-${engine}-query"
-#             echo "Job $job_counter: $engine $test $rest_of_line"
-#             $load_command
-#             $query_command
-#         else 
-#             echo "Invalid test type: $test at line $job_counter"
-#         fi
-#     else
-#         echo "Invalid engine type: $engine at line $job_counter"
-#     fi
-#     if [[ "$job_counter" == "$STOP_AFTER" ]]; then
-#         break
-#     fi
-#     ((job_counter++))
-# done < job_list.txt
+# Define the user-defined jobs
+# TODO: Complete the configuration of non-TPCDS jobs
+job_counter=1
+STOP_AFTER=1
+while IFS=' ' read -r engine test rest_of_line; do
+    # Check if the engine type is valid
+    if [[ "$engine" == "delta" || "$engine" == "iceberg" || "$engine" == "hudi" ]]; then
+        # Construct the job command based on the engine type and the rest of the line
+        if [[ "$test == tpcds" ]]; then
+            load_command="./run-benchmark.py \
+                --cluster-hostname ${cluster_hostname} \
+                -i ${pem_file} \
+                --ssh-user ${ssh_user} \
+                --source-path ${source_path} \
+                --benchmark-path /output/load-1gb \
+                --benchmark tpcds-${rest_of_line}-${engine}-load"
+            query_command="./run-benchmark.py \
+                --cluster-hostname ${cluster_hostname} \
+                -i ${pem_file} \
+                --ssh-user ${ssh_user} \
+                --benchmark-path /output/load-1gb \
+                --benchmark tpcds-${rest_of_line}-${engine}"
+            echo "Job $job_counter: $engine $test $rest_of_line"
+            $load_command
+            $query_command
+        elif [[ "$test == refresh" ]]; then
+            strategy=$rest_of_line[0]
+            run_command="./run-benchmark.py \
+                --cluster-hostname ${cluster_hostname} \
+                -i ${pem_file} \
+                --ssh-user ${ssh_user}"
+            echo "Job $job_counter: $engine $test $rest_of_line"
+            $run_command
+        elif [[ "$test == merge" ]]; then
+            echo "Job $job_counter: $engine $test $rest_of_line"
+        elif [[ "$test == count" ]]; then
+            load_command="./run-benchmark.py \
+                --cluster-hostname ${cluster_hostname} \
+                -i ${pem_file} \
+                --ssh-user ${ssh_user} \
+                --benchmark-path /output/sorted-${rest_of_line}-${engine} \
+                --benchmark ${rest_of_line}-files-sorted-${engine}-load"
+            query_command="./run-benchmark.py \
+                --cluster-hostname ${cluster_hostname} \
+                -i ${pem_file} \
+                --ssh-user ${ssh_user} \
+                --benchmark-path /output/sorted-${rest_of_line}-${engine} \
+                --cloud-provider hdfs \
+                --benchmark ${rest_of_line}-files-sorted-${engine}-query"
+            echo "Job $job_counter: $engine $test $rest_of_line"
+            $load_command
+            $query_command
+        else 
+            echo "Invalid test type: $test at line $job_counter"
+        fi
+    else
+        echo "Invalid engine type: $engine at line $job_counter"
+    fi
+    if [[ "$job_counter" == "$STOP_AFTER" ]]; then
+        break
+    fi
+    ((job_counter++))
+done < job_list.txt
 
 STATS_DIR="/home/$ssh_user/spark-stats"
 
@@ -192,6 +200,25 @@ wget -qO "${FOLDER_NAME}/api.json" http://localhost:18080/api/v1/applications
 
 # Get the application IDs and reverse the order to match the order of the jobs
 ids=$(jq -r '.[] | .id' "${FOLDER_NAME}/api.json" | tac)
-echo "$ids"
 
+# Read the job_list.txt file and store the task names in an array
+task_names=()
+while IFS= read -r line; do
+    task_name="${line// /_}"
+    task_names+=("$task_name")
+done < job_list.txt
 
+counter=0
+for id in $ids; do
+    # Find the corresponding task name for the current ID
+    task_name="${task_names[$counter]}"
+
+    # Create a subfolder with the task name
+    subfolder_name="${FOLDER_NAME}/${task_name}"
+    if [ ! -d "$subfolder_name" ]; then
+        mkdir -p "$subfolder_name"
+
+    fi
+
+    ((counter++))
+done
